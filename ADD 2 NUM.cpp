e87f5fcc5f3b9dabd26c75ea84a6f288aca8e647
sk shadik <57946665+SK-SHADIK@users.noreply.github.com>
2019-12-04 10:04:00 +0600

    ADD TWO NUMBER C++
    THIS IS THE CODE FOR ADD TWO NUMBER IN C++

---
+++ ADD 2 NUM.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int S,K,O;
+
+    cout<<"PLEASE ENTER TWO NUMBER :"<<endl;
+    cin>>S>>K;
+
+    O=S+K;
+
+    cout<<"THE ADD IS :"<<O<<endl;
+
+    return 0;
+}
