202269db722c14116364abfca1f00d230b98a16a
sk shadik <57946665+SK-SHADIK@users.noreply.github.com>
2019-12-04 10:07:59 +0600

    LENGTH OF STRING C++
    THIS IS THE CODE FOR LENGTH OF STRING IN C++

---
+++ LENGTH OF STRING.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str ;
+
+    cout<<"PLEASE ENTER A STRING :"<<endl;
+    cin>>str;
+
+    cout << "String Length = " << str.size();
+
+    return 0;
+}
